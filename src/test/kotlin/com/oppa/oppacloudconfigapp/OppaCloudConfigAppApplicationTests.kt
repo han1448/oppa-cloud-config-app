cc015c0115761946faa288bced536123bacc1c24
seunghyeon.han <seunghyeon.han@navercorp.com>
2021-08-01 11:15:32 +0900

    init

---
+++ src/test/kotlin/com/oppa/oppacloudconfigapp/OppaCloudConfigAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oppa.oppacloudconfigapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OppaCloudConfigAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
